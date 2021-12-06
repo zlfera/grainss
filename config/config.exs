acf29c061717ea99d46fd31456697619dda1af11
zlfera <zlfera@gmail.com>
2021-12-06 10:02:07 +0800

    up

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 ## This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+  # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
 config :grain_web,
